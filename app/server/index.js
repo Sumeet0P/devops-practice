53315b297310e666114e021210cf5aa7658de16f
Sumeet0P <schauhan8886@gmail.com>
2026-04-09 17:50:33 +0700

    add GitLab CI file and docker-compose

--- app/server/index.js
+++ app/server/index.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app's URL
-  origin: 'http://127.0.0.1:5173' // Your React app's URL
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+  ]
 }));
 
 app.get("/api", (req, res) => {
